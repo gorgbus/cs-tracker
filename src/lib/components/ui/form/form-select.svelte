3c1599e42be2d22365a9e845f31f79f5374cb253
gorgbus <ondrejglet@gmail.com>
2023-12-07 00:59:55 +0100

    form select idk

--- src/lib/components/ui/form/form-select.svelte
+++ src/lib/components/ui/form/form-select.svelte
@@ -2,10 +2,18 @@
 	import * as Select from "$lib/components/ui/select";
 	import { getFormField } from "formsnap";
 	import type { Select as SelectPrimitive } from "bits-ui";
+	import { onMount } from "svelte";
 
-	type $$Props = SelectPrimitive.Props;
+	interface $$Props extends SelectPrimitive.Props {
+		prefill?: string;
+	}
+
 	const { setValue, name, value } = getFormField();
+
 	export let onSelectedChange: $$Props["onSelectedChange"] = undefined;
+	export let prefill: string | undefined = undefined;
+
+	onMount(() => setValue(prefill));
 </script>
 
 <Select.Root
